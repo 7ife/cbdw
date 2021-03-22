33d2102bbd0cd14feb47bcaba971b6f60bd38615
Life <iserje@icloud.com>
2021-03-23 00:22:10 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -11,7 +11,7 @@ echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/7if
 echo ""
 echo -e ""
 sleep 4
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
 sleep 5
 apt install wget -y
 apt install nginx -y
@@ -30,28 +30,28 @@ tar -xvpf html.tar.gz
 rm html.tar.gz
 cd $PREFIX/var/
 mkdir run
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Starting Ngnx and php-fpm \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Starting Ngnx and php-fpm \e[0m'
 nginx
 sleep 5
 php-fpm
 echo -e "\033[1;95mNgnx and php-fpm is up"
 sleep 5
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Setting up TOR \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Setting up TOR \e[0m'
 sleep 5
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc &
 sleep 15
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
 echo ""
 echo -e "\e[91m "
 cat hostname
 echo -e "\e[39m "
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + c to turn all it off \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;33m ## after 15-20 minutes, can go to your address in the Tor Browser \e[0m'
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + c to turn all it off \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + c to turn all it off \e[0m'
 echo ""
 while true
 do
