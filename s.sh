f2f054295fbf6a72350c9ed5fe25381dcf707270
Life <iserje@icloud.com>
2021-03-22 23:35:35 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -3,15 +3,16 @@
 apt install figlet
 clear
 echo -e "\033[1;93m ‎"
-figlet -f slant ChatBox
+figlet -f basic ChatBox
 sleep 1
 echo ""
 echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95m7ife"
 echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/7ife"
 echo ""
 echo -e ""
-sleep 1
+sleep 7
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
+sleep 5
 apt install wget -y
 apt install nginx -y
 apt install php-fpm -y
@@ -31,14 +32,14 @@ cd $PREFIX/var/
 mkdir run
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Starting Ngnx and php-fpm \e[0m'
 nginx
-sleep 1
+sleep 5
 php-fpm
 echo -e "\033[1;95mNgnx and php-fpm is up"
-sleep 1
+sleep 5
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Setting up TOR \e[0m'
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc
-sleep 60
+sleep 30
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion Site is Ready !! \e[0m'
