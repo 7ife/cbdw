df3ca202a38bd92ff878d86c00c1e26090113da2
Life <iserje@icloud.com>
2021-03-23 01:57:43 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -2,6 +2,7 @@
 #Coded by 7ife
 apt install figlet
 clear
+echo ""
 echo -e "\033[1;93m ‎"
 figlet -f shadow CBDW
 sleep 3
@@ -11,10 +12,10 @@ echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/7if
 echo -e "              \033[1;97m  E-mail   \033[1;90m: \033[1;97m7ife@pm.me"
 echo ""
 echo -e ""
-sleep 5
-echo -e $'\e[1;33m[\e[0m\e[1;76m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
+sleep 3
+echo -e $'\e[1;35m[\e[0m\e[1;77m CBDW \e[0m\e[1;35m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
 echo ""
-sleep 5
+sleep 2
 apt install wget -y
 apt install nginx -y
 apt install php-fpm -y
@@ -31,24 +32,23 @@ rm html.tar.gz
 cd $PREFIX/var/
 mkdir run
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Starting Nginx & php-fpm \e[0m'
-sleep 4
-nginx
+echo -e $'\e[1;35m[\e[0m\e[1;77m CBDW \e[0m\e[1;35m]\e[0m\e[1;32m ## Starting Nginx & php-fpm \e[0m'
 sleep 2
+nginx
+sleep 1
 php-fpm
+echo -e "        \033[1;93m Nginx & php-fpm is up"
 echo ""
-echo -e "\033[1;96m Nginx & php-fpm is up"
+sleep 1
 echo ""
-sleep 3
-echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Starting TOR \e[0m'
-sleep 4
+echo -e $'\e[1;35m[\e[0m\e[1;77m CBDW \e[0m\e[1;35m]\e[0m\e[1;32m ## Starting TOR \e[0m'
+sleep 2
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc &
 sleep 33
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
+echo -e $'\e[1;35m[\e[0m\e[1;77m CBDW \e[0m\e[1;35m]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
 echo ""
 echo -e "\e[91m "
 cat hostname
@@ -56,7 +56,7 @@ echo -e "\e[39m "
 echo ""
 echo -e "\033[1;33mafter 15-20 minutes, can go to your address in the Tor Browser"
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m CBDW \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + C to turn all it off \e[0m'
+echo -e $'\e[1;35m[\e[0m\e[1;77m CBDW \e[0m\e[1;35m]\e[0m\e[1;32m ## Press CTRL + C to turn all it off \e[0m'
 echo ""
 while true
 do
