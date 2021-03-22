776759b0af99f432ba341cb81712c527e6410b69
Life <iserje@icloud.com>
2021-03-23 00:08:55 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 #Coded by 7ife
-apt install figlet
+apt install toilet
 clear
 echo -e "\033[1;93m ‎"
-figlet -f bigmono9 -F gay "ChatBox"
+toilet -f mono12 -F gay ChatBox
 sleep 1
 echo ""
 echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95m7ife"
@@ -37,18 +37,21 @@ php-fpm
 echo -e "\033[1;95mNgnx and php-fpm is up"
 sleep 5
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Setting up TOR \e[0m'
+sleep 5
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc &
-sleep 30
+sleep 15
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion Site is Ready !! \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
 echo ""
 echo -e "\e[91m "
 cat hostname
 echo -e "\e[39m "
 echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Press ctrl + c to Exit \e[0m'
+echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + c to turn all it off \e[0m'
+echo ""
+echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Press CTRL + c to turn all it off \e[0m'
 echo ""
 while true
 do
