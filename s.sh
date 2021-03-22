90df893079455d54633400e79dcff73a668cdcc0
Life <iserje@icloud.com>
2021-03-22 23:45:06 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -3,14 +3,14 @@
 apt install figlet
 clear
 echo -e "\033[1;93m ‎"
-figlet -f basic ChatBox
+figlet -f bigmono9 -F gay "ChatBox"
 sleep 1
 echo ""
 echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95m7ife"
 echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/7ife"
 echo ""
 echo -e ""
-sleep 7
+sleep 4
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m # Installing Dependencies ! \e[0m'
 sleep 5
 apt install wget -y
@@ -38,7 +38,7 @@ echo -e "\033[1;95mNgnx and php-fpm is up"
 sleep 5
 echo -e $'\e[1;33m[\e[0m\e[1;77m ChatBox \e[0m\e[1;33m]\e[0m\e[1;32m ## Setting up TOR \e[0m'
 cd $PREFIX/share/nginx/html/cbdw/onion/
-tor -f instance_torrc
+tor -f instance_torrc &
 sleep 30
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
