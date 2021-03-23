3bdda0581260713379d3037b1c6c8e9352f61e88
Life <iserje@icloud.com>
2021-03-23 14:16:53 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -35,7 +35,7 @@ search="admin_similar-to-password"
 read -p "#Enter Change name Admin panel >>> " replace
 if [[ $search != "" && $replace != "" ]]; then
 sed -i "s/$search/$replace/gi" index.html
-sed -i "s/$search/$replace/gi" /cbdw/www/
+sed -i "s/$search/$replace/gi" /cbdw/www/index.php
 fi
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo ""
@@ -43,7 +43,7 @@ echo -e " \033[1;93m successful"
 cd $PREFIX/var/
 mkdir run
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting Nginx & php-fpm \e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting Nginx & php-fpm\e[0m'
 sleep 2
 nginx
 sleep 1
@@ -52,14 +52,14 @@ echo -e " \033[1;93m Nginx & php-fpm is up"
 echo ""
 sleep 1
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting TOR \e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting TOR\e[0m'
 sleep 2
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc &
 sleep 33
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready! \e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready\e[0m'
 echo ""
 echo -e "\e[91m "
 cat hostname
@@ -67,7 +67,7 @@ echo -e "\e[39m "
 echo ""
 echo -e "\033[1;33m If Bootstrapped 100% after 15-20 minutes Can go to Your address in the Tor Browser"
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\033[1;90m # Press CTRL + C to close Tor \e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\033[1;90m # Press CTRL + C to close Tor\e[0m'
 echo ""
 while true
 do
