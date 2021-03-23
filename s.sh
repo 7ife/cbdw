f608ce8e6ca4b7e2bca3a8708296cb3beb4bf8dd
Life <iserje@icloud.com>
2021-03-23 21:30:05 +0600

    Add files via upload

--- s.sh
+++ s.sh
@@ -12,7 +12,8 @@ echo ""
 echo -e "\033[1;94m::ChatBox:for:DarkWeb::"
 echo ""
 echo -e ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Installing Dependencies\e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m #Installing Dependencies\e[0m'
+sleep 2
 apt install wget -y
 apt install nginx -y
 apt install php-fpm -y
@@ -43,7 +44,7 @@ echo -e " \033[1;93m successful\e[0m"
 echo -e "\033[1;95m ‎"
 #Enter change password for Admin
 search="pass-for-Admin"
-read -p "# Enter change password for Admin >>> \e[1;34m" passad
+read -p "# Enter change password for Admin >>> " passad
 if [[ $search != "" && $passad != "" ]]; then
 sed -i "s/$search/$passad/gi" cbdw/www/include/Display-ChatBox.php
 fi
@@ -52,7 +53,7 @@ echo -e " \033[1;93m successful"
 cd $PREFIX/var/
 mkdir run
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting Nginx & php-fpm\e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m #Starting Nginx & php-fpm\e[0m'
 sleep 2
 nginx
 php-fpm
@@ -60,14 +61,14 @@ echo -e " \033[1;93m Nginx & php-fpm is up"
 echo ""
 sleep 1
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Starting TOR\e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m #Starting TOR\e[0m'
 sleep 2
 cd $PREFIX/share/nginx/html/cbdw/onion/
 tor -f instance_torrc &
 sleep 33
 cd $PREFIX/share/nginx/html/cbdw/onion/tor-dm
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m # Your Onion domain for ChatBox is Ready\e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\e[1;32m #Your Onion domain for ChatBox is Ready\e[0m'
 echo ""
 echo -e "\e[91m "
 cat hostname
@@ -75,7 +76,7 @@ echo -e "\e[39m "
 echo ""
 echo -e "\033[1;33m If Bootstrapped 100% after 15-20 minutes Can go to Your address in the Tor Browser"
 echo ""
-echo -e $'\e[1;34m [ CBDW ]\e[0m\033[1;90m # Press CTRL + C to close Tor\e[0m'
+echo -e $'\e[1;34m [ CBDW ]\e[0m\033[1;90m #Press CTRL + C to close Tor\e[0m'
 echo ""
 while true
 do
